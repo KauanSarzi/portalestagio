257eda0bd1e66701c8a83a31f8d6cc69381196c3
Kauan Sarzi Da Rocha <kauansarzi57@gmail.com>
2026-04-17 12:46:56 -0300

    Delete g2-main directory

--- g2-main/webservicerest/src/main/java/com/portalestagio/webservicerest/repositories/AreaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.portalestagio.webservicerest.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.portalestagio.webservicerest.model.Area;
-
-public interface AreaRepository extends JpaRepository<Area, Long> {
-    
-}
--- g2-main/webservicerest/src/main/java/com/portalestagio/webservicerest/repositories/EmpresaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.portalestagio.webservicerest.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.portalestagio.webservicerest.model.Empresa;
-
-public interface EmpresaRepository extends JpaRepository<Empresa, Long> {
-    
-}
